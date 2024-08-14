fffdf958e0aba2429c657202b972c54daa804075
CalebOTurner <TTVAltra@gmail.com>
2024-08-14 18:02:43 +1200

    Taking break, Second Act, Questionnaire

--- app/src/main/java/com/example/fixitfitness/SecondActivity.java
+++ app/src/main/java/com/example/fixitfitness/SecondActivity.java
@@ -1,6 +1,11 @@
 package com.example.fixitfitness;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +25,47 @@ public class SecondActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void handleSubmit (View view) {
+        Log.d("2nd Activity", "The button is clicked");
+
+        //need to get user edits from each of the text fields
+
+        EditText nameEdit = findViewById(R.id.Name); //name edit
+        EditText ageEdit = findViewById(R.id.Age); //age edit
+        EditText weightEdit = findViewById(R.id.Weight); // weight edit
+        EditText heightEdit = findViewById(R.id.Height); //height edit
+
+        String name = nameEdit.getText().toString();
+        String name = ageEdit.getText().toString();
+        String name = weightEdit.getText().toString();
+        String name = heightEdit.getText().toString();
+
+        //validate the inputs
+        // show error messages if applicable
+        if (name.length() <= 0) {
+            showPopup(this, "Please enter a name");
+            return;
+        }
+
+        if (name.length() => 20) {
+            showPopup(this, "Name is too long");
+            return;
+        }
+        if (age.length() <= 0) {
+            showPopup(this, "Please enter an age");
+            return;
+        }
+        int ageInt;
+        try {
+            ageInt = Integer.parseInt(age);
+            if (ageInt < 0) {
+                showPopup(this, "Please enter a valid age");
+                return;
+            }
+        } catch (NumberFormatException)
+    }
+    public static void showPopup(Context context, String errorMessage) {
+        Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show();
     }
 }
